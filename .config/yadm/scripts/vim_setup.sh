38c1a2a89cf18faa6758e147c4923d6292a4884b
Ryan Luckie <rluckie@cisco.com>
2020-06-25 08:36:39 +0000

    x

--- .config/yadm/scripts/vim_setup.sh
+++ .config/yadm/scripts/vim_setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-vim +PlugUpgrade +PlugClean! +PlugUpdate +qa --not-a-term > /dev/null 2>&1
+echo '\n' | vim +PlugUpgrade +PlugClean! +PlugUpdate +qa --not-a-term > /dev/null 2>&1
